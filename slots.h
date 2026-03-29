4ce655cd8a90eb7d815d3d0c287461ecd76ea406
aodaniel2128 <aodaniel2128@eagle.fgcu.edu>
2026-03-29 15:51:10 -0400

    Update slots.cpp
    Here is the slot.cpp code. Let me know what you guys think and what adjustments need to be made.

--- slots.h
+++ slots.h
@@ -2,3 +2,23 @@
 #include <format>
 #include <vector>
 using namespace std; 
+
+#include <random>
+#include <format>
+#include <vector>
+using namespace std; 
+
+void Slots::play() override {
+        int reelOutcome = threeRandomIntegers(); // set reel outcome equal to the threeRandomIntegers function. This will display the slot results and determine 
+      // whether to return 1 or 0
+        if (reelOutcome == 1) { // player wins if outcome is equal to one
+            balance += 10'000.0;
+            cout << "You win! $10'000.0  won!\n";
+            ++winningMatch;
+        }
+        else {
+            balance -= 1'000.0; // player loses if outcome is less than zero
+            cout << "You lose! $1'000 lost!\n";
+            ++losingMatch;
+        }
+    }
