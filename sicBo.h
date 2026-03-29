a6cd622b4a0e1ccfd9761ff36a3b2f44ff74e30e
aodaniel2128 <aodaniel2128@eagle.fgcu.edu>
2026-03-29 15:37:18 -0400

    Update sicBo.h
    I have added sicBo.h to the GitHub repository. Please give it a review and provide feedback when you have a moment.

--- sicBo.h
+++ sicBo.h
@@ -4,7 +4,7 @@
 class sicBo : public Game {
 private:
 	int m_winningMatch = 0, m_losingMatch = 0, m_diceNumber = 0, m_dice1 = 0, m_dice2 = 0, m_dice3 = 0, m_bet = 0;
-public:
+public: 
 	void play() override;  
 	double singleDieBet() const;
 	double dieCombinations() const;
