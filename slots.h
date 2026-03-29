c6b9a4e234a01e96c1a47876bb7b9591889c4eb1
aodaniel2128 <aodaniel2128@eagle.fgcu.edu>
2026-03-29 15:00:15 -0400

    Create slots.h

---
+++ slots.h
@@ -0,0 +1,11 @@
+// slots class derived from Player class
+
+class Slots : public Game {
+private:
+	int m_winningMatch{ 0 }, m_losingMatch{ 0 }, m_reelOutcome{ 0 };
+public:
+	void play() override;
+	double spinReel const();  
+};
+
+ 
