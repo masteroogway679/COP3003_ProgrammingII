3ed450a5224d4309c51a3a73a5b34e16f5f5865b
aodaniel2128 <aodaniel2128@eagle.fgcu.edu>
2026-04-05 23:06:00 -0400

    Here's an updated version of slots.h.
    Simpler code version that only focuses on matching three symbols

--- slots.h
+++ slots.h
@@ -1,13 +1,13 @@
- // slots class derived from Player class
+ 
+// slots class derived from Game class
 #pragma once
-#include "player.h"
+#include "Game.h"
 
 class Slots : public Game {
 private:
-	int m_winningMatch{ 0 }, m_losingMatch{ 0 }, m_reelOutcome{ 0 };
+int winCount, randomInteger2();
 public:
 	void play() override;
-	double spinReel const();  
 };
 
  
