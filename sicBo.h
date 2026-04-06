0259a00a847e8ca04e53359036faea9266ba66df
joelcon1 <another123222@gmail.com>
2026-04-05 21:52:55 -0400

    Update sicBo.h
    #pragma once - typo fixed
    class SicBo - typo fixed (capitalization)
    randomInteger is now declared
    Constructor added

--- sicBo.h
+++ sicBo.h
@@ -1,12 +1,16 @@
-#progma once
+#pragma once
 #include "Game.h"
 
-class sicBo : public Game {
+class SicBo : public Game {
 private:
-	int winningMatch, losingMatch, outcome, choice, frequency, dice_number, dice1, dice2, dice3, bet;
-public: 
-	void play() override;  
-	double singleDieBet() const;
-	double dieCombinations() const;
-	double dieTotals() const;
+    int winningMatch, losingMatch, outcome, choice, frequency, dice_number, dice1, dice2, dice3, bet, randomInteger();
+public:
+    SicBo(Player* p) : Game(p) {}
+
+    void play() override;
+    double singleDieBet() const;
+    double dieCombinations() const;
+    double dieTotals() const;
+
+
 };
