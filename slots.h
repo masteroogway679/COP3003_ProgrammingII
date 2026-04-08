95a8ba3031816f1fbd5c702f34ac721548333023
joelcon1 <another123222@gmail.com>
2026-04-07 20:10:12 -0400

    Update slots.h
    removed #include slots.h from header in .h file
    added #pragma once
    Added slots class (inherits from game)
    Added slots constructor
    ONLY defined function play() override (removed .cpp code)
    defined function randomInteger()

--- slots.h
+++ slots.h
@@ -1,20 +1,13 @@
-#include <random>
-#include <format>
-#include <vector>
-#include "slots.h"
-using namespace std; 
+#pragma once
 
-void Slots::play() override {
-        int reelOutcome = threeRandomIntegers(); // set reel outcome equal to the threeRandomIntegers function. This will display the slot results and determine 
-      // whether to return 1 or 0
-        if (reelOutcome == 1) { // player wins if outcome is equal to one
-            balance += 10'000.0;
-            cout << "You win! $10'000.0  won!\n";
-            ++winningMatch;
-        }
-        else {
-            balance -= 1'000.0; // player loses if outcome is less than zero
-            cout << "You lose! $1'000 lost!\n";
-            ++losingMatch;
-        }
-    }
+#include "Game.h"
+
+class Slots : public Game {
+private:
+    int randomInteger();
+
+public:
+    Slots(Player* p) : Game(p) {}
+
+    void play() override;
+};
